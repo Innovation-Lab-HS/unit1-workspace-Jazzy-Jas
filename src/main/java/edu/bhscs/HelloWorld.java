1f27a4c4d8fc085571c5303a67c392719a7791d1
Jazzy-Jas <2014380@apps.nsd.org>
2024-09-13 18:22:04 +0000

    ......

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld{
+  // all code in Java sits inside a class
+
+  //every Javav progrm needs a public main method
+  //for the next while, ALL code you write will live inside main()
+  public static void main(String[] args) {
+  System.out.println("Hello.");
+  }
+}
